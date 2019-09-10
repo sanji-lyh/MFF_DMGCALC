2d09d88e97718fec75a7dbd43b18d4e05b4c4621
sanji-lyh <52849007+sanji-lyh@users.noreply.github.com>
2019-09-10 22:30:46 +0800

    v1.3 - Sort supreme by elements

--- js/data.js
+++ js/data.js
@@ -13,11 +13,22 @@ function GetAbility(callback){
 	$.getJSON('./data/ability.json', function(data) {         
 		abilityList = data;
 		
-		for(i=(abilityList.length-1); i >= 0; i--){
-			$('#ability_template').append($('<option>', {value:i, text:(abilityList[i]["AbilityName"]+" -- Atk: " + abilityList[i]["Attack"])}));
+		// 1 = fire, 2 = water, 3 = wind, 4 = earth, 5 = light, 6 = dark
+		var elem_text = ["Fire", "Water", "Wind", "Earth", "Light", "Dark"];
+		
+		
+		for(i=1; i<=6; i++){
+			$('#ability_template').append($('<optgroup/>').attr('label', elem_text[i-1]));
+			
+			for(j=(abilityList.length-1); j >= 0; j--){
+				if(abilityList[j]["Elem"] == i) {
+					$('#ability_template').find('optgroup').last().append($('<option>', {value:j, text:(abilityList[j]["AbilityName"]+" -- Atk: " + abilityList[j]["Attack"])}));
+				}
+			}
 		}
 		
-		$('#ability_template').append($('<option>', {value:-1, text:"Custom input ability"}));
+		$('#ability_template').append($('<optgroup/>').attr('label', "Custom"));
+		$('#ability_template').find('optgroup').last().append($('<option>', {value:-1, text:"Custom input ability"}));
 		
 		callback();
 	});
